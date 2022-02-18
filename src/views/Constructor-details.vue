7292f1bbc98e62bc7e897f436b923634ad7b04fa
Fábry Dávid <fabry.david@developit.hu>
2022-02-18 13:26:43 +0100

    constructors layout

--- src/views/Constructor-details.vue
+++ src/views/Constructor-details.vue
@@ -1,63 +1,69 @@
 <template>
   <loading-indicator v-if="isLoading" />
 
-  <div v-else class="constructor-details">
-    <div class="constructor-name">
-      <h2 :style="{ color: team.color.primary }">
-        {{ this.team.nameExtended.shortName }}
-      </h2>
-      <div :style="{ color: team.color.tertiary }" class="name-second-part">
-        {{ getConstructorNameSecondPart(team.nameExtended.fullName) }}
-      </div>
-    </div>
-    <v-container class="relative content">
-      <div class="fill-width d-flex flex-column justify-center align-center">
+  <v-container v-else class="constructor-details pa-0" fluid>
+    <v-row class="my-0 mx-3 content-row">
+      <v-col class="d-flex align-end" cols="2">
+        <div class="constructor-name">
+          <div :style="{ color: team.color.tertiary }" class="name-second-part">
+            {{ getConstructorNameSecondPart(team.nameExtended.fullName) }}
+          </div>
+          <h2 :style="{ color: team.color.primary }">
+            {{ this.team.nameExtended.shortName }}
+          </h2>
+        </div>
+      </v-col>
+      <v-col class="d-flex align-end justify-center pb-0" cols="8">
         <constructor-details-background
           :primary="team.color.primary"
           :secondary="team.color.secondary"
           :tertiary="team.color.tertiary"
-          style="width: 80%;"
         />
+      </v-col>
+      <v-col class="d-flex align-end" cols="2">
+        <div class="stats">
+          <div class="stat-item">
+            <div :style="{ color: team.color.tertiary }" class="stat-title">
+              wins
+            </div>
+            <div :style="{ color: team.color.primary }" class="stat-value">
+              {{ team.wins }}
+            </div>
+          </div>
 
-        <div class="car-container d-flex justify-center">
+          <div class="stat-item">
+            <div :style="{ color: team.color.tertiary }" class="stat-title">
+              points
+            </div>
+            <div :style="{ color: team.color.primary }" class="stat-value">
+              {{ team.points }}
+            </div>
+          </div>
+
+          <div class="stat-item">
+            <div :style="{ color: team.color.tertiary }" class="stat-title">
+              standing
+            </div>
+            <div :style="{ color: team.color.primary }" class="stat-value">
+              {{ team.position }}
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <div class="bottom relative">
+      <v-row justify="center">
+        <v-col cols="7" class="pa-0">
           <img
-            alt="Car"
-            class="car"
+            class="car-image"
             src="@/assets/images/cars/mercedes.png"
+            alt="Car"
           />
-        </div>
-      </div>
-    </v-container>
-
-    <div class="stats">
-      <div class="stat-item">
-        <div :style="{ color: team.color.tertiary }" class="stat-title">
-          wins
-        </div>
-        <div :style="{ color: team.color.primary }" class="stat-value">
-          {{ team.wins }}
-        </div>
-      </div>
-
-      <div class="stat-item">
-        <div :style="{ color: team.color.tertiary }" class="stat-title">
-          points
-        </div>
-        <div :style="{ color: team.color.primary }" class="stat-value">
-          {{ team.points }}
-        </div>
-      </div>
-
-      <div class="stat-item">
-        <div :style="{ color: team.color.tertiary }" class="stat-title">
-          standing
-        </div>
-        <div :style="{ color: team.color.primary }" class="stat-value">
-          {{ team.position }}
-        </div>
-      </div>
+        </v-col>
+      </v-row>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -68,7 +74,7 @@ import ConstructorDetailsBackground from "@/components/constructors/constructor-
 
 export default {
   name: "Constructor-details",
-  components: { ConstructorDetailsBackground, LoadingIndicator },
+  components: { LoadingIndicator, ConstructorDetailsBackground },
   data: () => ({
     isLoading: false,
     constructorId: null,
@@ -114,74 +120,66 @@ export default {
 
 <style lang="scss" scoped>
 .constructor-details {
-  $side-offset: 20px;
-
+  $floor-height: 115px;
   min-height: 93vh;
 
+  .content-row {
+    min-height: calc(93vh - #{$floor-height});
+  }
+
   .constructor-name {
-    transform: rotate(-90deg);
-    position: absolute;
-    bottom: 0;
-    left: $side-offset;
-    transform-origin: top left;
-    z-index: 5;
-  }
+    transform: scale(-1);
+    writing-mode: vertical-lr;
+    text-orientation: mixed;
 
-  h2,
-  .name-second-part {
-    font-weight: 900;
-    text-transform: uppercase;
-    text-shadow: 0 2px 11px rgba(0, 0, 0, 0.2);
-  }
+    h2,
+    .name-second-part {
+      font-weight: 900;
+      text-transform: uppercase;
+      text-shadow: 0 2px 11px rgba(0, 0, 0, 0.2);
+    }
 
-  h2 {
-    font-size: 100px;
-    line-height: 80px;
-  }
+    h2 {
+      font-size: 100px;
+      line-height: 80px;
+    }
 
-  .name-second-part {
-    font-size: 40px;
-    line-height: 60px;
+    .name-second-part {
+      font-size: 40px;
+      line-height: 60px;
+    }
   }
 
   .stats {
-    width: fit-content;
-    position: absolute;
-    z-index: 5;
-    right: $side-offset;
-    top: 0;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+    text-align: right;
 
     .stat-item {
       text-transform: uppercase;
+      margin-top: 50px;
       filter: drop-shadow(2px 0px 11px rgba(0, 0, 0, 0.2));
 
       .stat-title {
         font-size: 30px;
-        line-height: 45px;
       }
 
       .stat-value {
         margin-top: -30px;
         font-weight: 900;
-        font-size: 90px;
-        line-height: 150px;
+        font-size: 70px;
       }
     }
   }
 
-  .content{
-    margin-left: -10%;
-  }
+  .bottom {
+    min-height: $floor-height;
+    box-shadow: 0px -3px 41px rgba(0, 0, 0, 0.19);
+    background-color: white;
 
-  .car-container {
-    width: 100vw;
-    background: #ffffff;
-    box-shadow: 0px -3px 41px rgb(0 0 0 / 19%);
-    min-height: 115px;
+    .car-image {
+      width: 100%;
+      margin-top: -100%;
+      filter: drop-shadow(2px 1px 11px rgba(0, 0, 0, 0.47));
+    }
   }
 }
 </style>
