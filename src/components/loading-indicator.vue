e56e603e245c022bcfd78bd9e2e6f409e89e4e9c
Fábry Dávid <fabry.david@developit.hu>
2022-02-21 12:14:51 +0100

    loading indicator

--- src/components/loading-indicator.vue
+++ src/components/loading-indicator.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="fill-height fill-width d-flex justify-center align-center">
-    <v-progress-circular indeterminate color="primary" />
+    <v-progress-circular v-if="!image" indeterminate color="primary" />
+    <img class="loading-indicator" v-else :src="image" alt="Loading..." />
   </div>
 </template>
 
 <script>
 export default {
   name: "loading-indicator",
+  data: () => ({
+    image: null,
+  }),
+  mounted() {
+    const tires = ["hard", "inter", "medium", "rain", "soft"];
+    const tire = tires[Math.floor(Math.random() * tires.length)];
+
+    this.image = require(`@/assets/images/tires/${tire}.svg`);
+  },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.loading-indicator {
+  width: 75px;
+  aspect-ratio: 1/1;
+  animation: rotate infinite 2s linear;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
